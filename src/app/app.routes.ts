56998bf2108b7cbab8f5d5304fcd38c445926bb9
BekerDi <143925720+BekerDi@users.noreply.github.com>
2026-02-07 12:19:46 +0300

    Update HTML layout for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home';
+import { CoursesComponent } from './pages/courses/courses';
+import { AboutComponent } from './pages/about/about';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' },
+];
