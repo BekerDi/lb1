5fa8d913bf3e083e89221db6eb34b56c40991f14
BekerDi <143925720+BekerDi@users.noreply.github.com>
2026-02-07 12:50:32 +0300

    Enable navigation with RouterLink in page components

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
-export class CoursesComponent {
-
-}
+export class CoursesComponent {}
