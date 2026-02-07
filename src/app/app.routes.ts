34ab8c7b499e5187621fb31cc67d07a1fbf4ed72
BekerDi <143925720+BekerDi@users.noreply.github.com>
2026-02-07 13:51:25 +0300

    Meaningful commit message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { CoursesComponent } from './pages/courses/courses';
 import { AboutComponent } from './pages/about/about';
-
+import {NotFoundComponent} from './pages/not-found/not-found';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'courses', component: CoursesComponent },
   { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', component: NotFoundComponent },
 ];
